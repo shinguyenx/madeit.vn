451009e5192417da322dc4975ca9dd2a4e5cda23
Shi Nguyen <shi.ngyenxuan@gmail.com>
2012-09-01 22:42:07 +0700

    Up code
    Code first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadeitVn::Application.config.secret_token = '0226da934bf7add89f2090d60f6efbcf1687ad66d26519caa34fe25f4f51ae4fb2433d3c6f018e85f7916e5802789990ee1a919716847acd1422d389b9f246b3'
